f9916a7aa0b94eebe3f48835c134565b3af24bcd
hra7s <216ec1464@gmail.com>
2024-02-09 10:29:29 +0530

    html5 data

---
+++ DATATYPES/index.js
@@ -0,0 +1,64 @@
+//array holds the data of different data types 
+
+
+// let arr=[1,2,3,4,5,6]
+// console.log(arr)
+
+// let arr2=[1,"hello","js", true,[1,2,3,4],{NAME:"HELLO"}]
+
+
+
+//how to access the values of array 
+
+// indexing 
+
+// let arr=[1,2,3,4,5,6]
+
+// console.log(arr[3])
+
+
+// let arr2=[1,"hello","js", true,[1,2,3,4],{NAME:"HELLO"}]
+
+// console.log(arr2[4][3])
+
+let arr2=[1,"hello","js", true,[1,2,3,4],{NAME:"HELLO"}]
+
+//length 
+
+// let out=arr2.length
+// console.log(out)
+
+//push 
+// let out=arr2.push(false)
+// console.log(out)
+// console.log(arr2)
+
+//pop 
+
+// let out=arr2.pop()
+// console.log(out)
+// console.log(arr2)
+
+//shift
+
+// let out=arr2.shift()
+// console.log(out)
+// console.log(arr2)
+
+//unshift 
+
+// let out=arr2.unshift("react")
+// console.log(out)
+// console.log(arr2)
+
+
+
+//concat 
+// let arr=[null,undefined,1,2,3,"four"]
+
+// let out=arr2.concat(arr)
+// console.log(out)
+// console.log(arr2) 
+
+// map reduce and filter 
+
